51beaeddedba7defbe6891099f8ad0877398d85a
Lark Sigmuond A. Babao <129244163+Snorlark@users.noreply.github.com>
2023-05-08 12:59:22 +0800

    Delete Babao_Long Exam directory

--- Babao_Long Exam/Iloilo.java
+++
@@ -1,9 +0,0 @@
-public class Iloilo implements Locations {
-
-    int airFare = 250;
-
-    @Override
-    public void accept(Tourist tourist) {
-        tourist.visit(this);
-    }
-}
--- Babao_Long Exam/Manila.java
+++
@@ -1,9 +0,0 @@
-public class Manila implements Locations {
-
-    int airFare = 50;
-
-    @Override
-    public void accept(Tourist tourist) {
-        tourist.visit(this);
-    }
-}
